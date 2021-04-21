e1d117abc9fa1a5056e9d69cd45268433f0e8020
Manoel Moura <70218216+Matsuo-Kage@users.noreply.github.com>
2021-04-21 15:14:03 -0300

    Add files via upload

--- g.js
+++ g.js
@@ -11,6 +11,12 @@ window.onload = function jogo(){
     grama.src = "Gramado.png"
     var monstro = new Image()
     monstro.src = "sapo-f.png"
+    var espada = new Image()
+    espada.src = "hit.png"
+    var nohitsound = document.createElement('audio')
+    nohitsound.src = "nohit.mp3"
+    var adagatotalsprite
+    var countAnim = 0
 
     //objetos
     var sprites = []
@@ -19,6 +25,8 @@ window.onload = function jogo(){
         img: grama,
         x: 0,
         y: 0,
+        spriteX: 0,
+        spriteY: 0,
         width: 3200,
         height: 3200,
         tamX: 3200,
@@ -30,22 +38,23 @@ window.onload = function jogo(){
         img: player,
         x: 0,
         y: 0,
+        spriteX: 0,
+        spriteY: 0,
         width: 160,
         height: 160,
         tamX: 160,
         tamY: 160,
         vel: 5,
-        vida: 10,
-        mana: 10,
-        stamina: 10,
         dir: 'up'
     }
     sprites.push(char)
 
     var sapo = {
         img: monstro,
-        x: Math.floor(Math.random() * mundo.width) + 1 ,
-        y: Math.floor(Math.random() * mundo.height) + 1 ,
+        x: 300,//Math.floor(Math.random() * mundo.width) + 1,
+        y: 300,//Math.floor(Math.random() * mundo.height) + 1,
+        spriteX: 0,
+        spriteY: 0,
         width: 40,
         height: 40,
         tamX: 80,
@@ -54,6 +63,56 @@ window.onload = function jogo(){
         vida: 10
     }
     sprites.push(sapo)
+
+    var bars = []
+
+    var vida = {
+        x: 35,
+        y: -30,
+        pontos: 100,
+        max: 100,
+        largura: 8,
+        regen: 0.5
+    }
+    bars.push(vida)
+
+    var magika = {
+        x: 35,
+        y: -20,
+        pontos: 100,
+        max: 100,
+        largura: 4,
+        regen: 1
+    }
+    bars.push(magika)
+
+    var stamina = {
+        x: 35,
+        y: -15,
+        pontos: 100,
+        max: 100,
+        largura: 4,
+        regen: 3
+    }
+    bars.push(stamina)
+
+
+    //golpes
+    var attack = []
+
+    var adaga = {
+        img: espada,
+        x: 1600,
+        y: 1600,
+        spriteX: 4,
+        spriteY: 0,
+        width: 160,
+        height: 160,
+        tamX: 160,
+        tamY: 160,
+        numSprites: 5
+    }
+    attack.push(adaga)
     
     //centralizar player
     char.x = (mundo.width - char.width)/2
@@ -83,7 +142,7 @@ window.onload = function jogo(){
     cam.y = (mundo.height - cam.height)/2
 
     //mover personagem
-    var mvLeft = mvRight = mvUp = mvDown = false
+    var mvLeft = mvRight = mvUp = mvDown = mvAtk = false
     window.addEventListener('keydown',function (e){
         var key = e.keyCode
         switch (key) {
@@ -120,13 +179,31 @@ window.onload = function jogo(){
             case 40:
                 mvDown = false;
                 break;
+            case 67:
+                bater()
+                if (stamina.pontos >= 10) {
+                    stamina.pontos -= 10
+                }
+                else 
+                    vida.pontos -= 5
+                break;
+            case 88:
+                alert(tempo)
+                break;
+            case 90:
+                vida.pontos += 10
+                break;    
         
             default:
                 break;
         }
     },false);
 
-    function loop () {
+    
+
+    
+
+    function loop() {
         window.requestAnimationFrame(loop,tela)
         update()
         render()
@@ -135,15 +212,23 @@ window.onload = function jogo(){
     function update() {
         if(mvLeft && !mvRight){
 			char.x -= char.vel;
+            char.spriteX = 3;
+            adaga.spriteY = 1;
 		}
 		if(mvRight && !mvLeft){
 			char.x += char.vel;
+            char.spriteX = 1;
+            adaga.spriteY = 3;
 		}
 		if(mvUp && !mvDown){
 			char.y -= char.vel;
+            char.spriteX = 2;
+            adaga.spriteY = 2;
 		}
 		if(mvDown && !mvUp){
 			char.y += char.vel;
+            char.spriteX = 0;
+            adaga.spriteY = 0;
 		}
 
         //limite do player
@@ -187,6 +272,35 @@ window.onload = function jogo(){
 		if(cam.y + cam.height > mundo.height){
 			cam.y = mundo.height - cam.height;
 		}
+
+        //lógica das barras
+        if (vida.pontos > vida.max) {
+            vida.pontos = vida.max  
+        }
+        else
+        if (vida.pontos > 0 && vida.pontos < vida.max) {
+            vida.pontos += vida.regen * 0.05
+        }
+        if (vida.pontos <= 0) {
+            alert('game over')
+        }
+        //magika
+        if (magika.pontos > magika.max) {
+            magika.pontos = magika.max  
+        }
+        else
+        if (magika.pontos > 0 && magika.pontos < magika.max) {
+            magika.pontos += magika.regen * 0.05
+        }
+        //stamina
+        if (stamina.pontos > stamina.max) {
+            stamina.pontos = stamina.max  
+        }
+        else
+        if (stamina.pontos > 0 && stamina.pontos < stamina.max) {
+            stamina.pontos += stamina.regen * 0.05
+        }
+        
     }
     
     function render() {
@@ -194,11 +308,34 @@ window.onload = function jogo(){
         ctx.translate(-cam.x,-cam.y)
         for(var i in sprites) {
             var spr = sprites[i]
-            ctx.drawImage(spr.img,0,0,spr.width,spr.height,spr.x,spr.y,spr.tamX,spr.tamY)
+            ctx.drawImage(spr.img,spr.spriteX * spr.width,spr.spriteY * spr.height,spr.width,spr.height,spr.x,spr.y,spr.tamX,spr.tamY)
         }
-        ctx.restore()
-        
-    }
+        for(var b in bars) {
+            var bar = bars[b]
+            if(b == 0){ctx.fillStyle = "#ff0000"}
+            else if(b == 1){ctx.fillStyle = "#0000ff"}
+            else if(b == 2){ctx.fillStyle = "#00ff00"}
+            ctx.fillRect(bar.x + char.x, bar.y + char.y, bar.pontos,bar.largura)
+            }
+        /*for (var a in attack) {
+            var atk = attack[a]
+            ctx.drawImage(atk.img, atk.spriteX * atk.width, atk.spriteY * atk.height, atk.height, atk.width, atk.x, atk.y, atk.tamX, atk.tamY)
+        }
+        if(1 = 1){
 
-    loop()
-}
+        }*/    
+        
+        
+        ctx.restore()
+        }
+        loop()
+
+        function bater() {
+            if (1 == 1) {
+            
+            }
+            nohitsound.play()
+            }
+        }
+
+    
